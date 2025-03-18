368bc39f017fafae9badb50930eb0d427340bf9d
KrisBoris <k.store@interia.eu>
2025-03-18 18:06:48 +0100

    Example 55
    Magic methods

--- Python-Advanced-Course/examples_51_60/example_55_magic_methods.py
+++ Python-Advanced-Course/examples_51_60/example_55_magic_methods.py
@@ -15,11 +15,48 @@ class Book:
         else:
             return False
 
+    def __lt__(self, other):
+        if self.pages < other.pages:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.pages > other.pages:
+            return True
+        else:
+            return False
+
+    def __add__(self, other):
+        return self.pages + other.pages
+
+    def __contains__(self, item):
+        if item in self.title or item in self.author:
+            return True
+        else:
+            return False
+
+    def __getitem__(self, key):
+        match key:
+            case "title":
+                return self.title
+            case "author":
+                return self.author
+            case "pages":
+                return self.pages
+            case _:
+                return f"Key {key} is invalid"
+
 
 book1 = Book("Sherlock Holmes", "Sir Arthur Conan Doyle", 335)
 book2 = Book("Herkules Poirot", "Agatha Christine", 210)
 book3 = Book("The Lord of The Rings", "J.R.R. Tolkien", 421)
 book4 = Book("The Lord of The Rings", "J.R.R. Tolkien", 421)
 
+print(book3)
 print(book3 == book4)
-
+print(book1 > book2)
+print(book2 < book1)
+print(book1 + book2)
+print("Rings" in book3)
+print(book1["title"])
